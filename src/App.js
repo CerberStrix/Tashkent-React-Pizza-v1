a3f464c7e988fbaa9e98da2f057bb8666c1cb470
Alisher Kim <cerberstrix@gmail.com>
2023-03-23 16:56:13 +0500

    add redux hooks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
+
 import { Route, Routes } from 'react-router-dom';
 import { Header } from './components';
 import { Home, Cart } from './pages';
+import { setPizzas } from './redux/action/pizzas';
 
 function App() {
-  const [pizzas, setPizzas] = React.useState([]);
+  const dispatch = useDispatch();
+  const { items } = useSelector(({ pizzas, filter }) => {
+    return {
+      items: pizzas.items,
+      sortBy: filter.sortBy,
+    };
+  });
+
   React.useEffect(() => {
     axios.get('http://localhost:3000/db.json').then(({ data }) => {
-      setPizzas(data.pizzas);
+      dispatch(setPizzas(data.pizzas));
     });
-  }, []);
+  }, [dispatch]);
 
   return (
     <div>
@@ -18,7 +28,7 @@ function App() {
         <Header />
         <div className="content">
           <Routes>
-            <Route path="/" element={<Home items={pizzas} />} />
+            <Route path="/" element={<Home items={items} />} />
             <Route path="/cart" element={<Cart />} />
           </Routes>
         </div>
