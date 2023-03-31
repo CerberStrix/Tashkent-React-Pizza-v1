f1fc1a50be76412b152fda0ac2e088ea848107bc
Alisher Kim <cerberstrix@gmail.com>
2023-03-31 19:07:22 +0500

    add server script for heroku

---
+++ server.js
@@ -0,0 +1,19 @@
+import * as dotenv from 'dotenv';
+
+const jsonServer = require('json-server');
+
+dotenv.config();
+const server = jsonServer.create();
+const router = jsonServer.router('./public/db.json');
+const middlewares = jsonServer.defaults({
+  static: './build',
+});
+
+const PORT = process.env.PORT || 3001;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(PORT, () => {
+  console.log('Server is running');
+});
